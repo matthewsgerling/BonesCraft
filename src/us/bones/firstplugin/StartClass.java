cfcad4d5509a0dcee11cdb12dc50bad991d9ba71
Matthew Gerling <matthewsgerling@gmail.com>
2020-03-02 22:02:27 -0600

    Blank Plugin
    First Blank Plugin

---
+++ src/us/bones/firstplugin/StartClass.java
@@ -0,0 +1,18 @@
+package us.bones.firstplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+class Main extends JavaPlugin {
+
+
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
