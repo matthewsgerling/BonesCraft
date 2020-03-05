a9b7bca32167e66964be6d1c0925515b6ef4bc49
Matthew Gerling <matthewsgerling@gmail.com>
2020-03-05 16:17:58 -0600

    Simple command implemented and codded

---
+++ src/us/bones/firstplugin/PlayerListener.java
@@ -0,0 +1,29 @@
+package us.bones.firstplugin;
+import org.bukkit.event.EventHandler;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import org.bukkit.event.player.PlayerQuitEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author Dinnerbone
+ */
+public class PlayerListener implements Listener {
+    private final BonesPlugin plugin;
+
+    public PlayerListener(BonesPlugin instance) {
+        plugin = instance;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        plugin.getLogger().info(event.getPlayer().getName() + " joined the server! :D");
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        plugin.getLogger().info(event.getPlayer().getName() + " left the server! :'(");
+    }
+}
